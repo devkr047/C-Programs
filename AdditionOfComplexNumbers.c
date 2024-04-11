0eeb99f99569ecbb19a6cdb24159ef2cd7c3abc0
KapilSinghMaan <166300100+KapilSinghMaan@users.noreply.github.com>
2024-04-11 20:10:10 +0530

    Update AdditionOfComplexNumbers.c

--- AdditionOfComplexNumbers.c
+++ AdditionOfComplexNumbers.c
@@ -6,12 +6,12 @@ void main()
     printf ("Enter Details of First Number\n\n");
     printf ("Enter Real Coefficient:");
     scanf ("%d",&real_n1);
-    printf ("Enter Imiginary Coefficient:");
+    printf ("Enter Imaginary Coefficient:");
     scanf ("%d",&img_n1);
     printf ("\nEnter Details of Second Number\n\n");
     printf ("Enter Real Coefficient:");
     scanf ("%d",&real_n2);
-    printf ("Enter Imiginary Coefficient:");
+    printf ("Enter Imaginary Coefficient:");
     scanf ("%d",&img_n2);
     printf ("\nFirst Number:%d+%di",real_n1,img_n1);
     printf ("\nSecond Number:%d+%di",real_n2,img_n2);
